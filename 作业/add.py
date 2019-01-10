dde72e24341ba6baed607b4402c4d032e13fca25
twuter <molin27881@163.com>
2019-01-10 22:35:29 +0800

    010作业

---
+++ 作业/add.py
@@ -0,0 +1,20 @@
+
+
+
+def add(a,b):
+    return a+b
+
+
+def muit(a,b):
+    return a - b
+
+def shen(a,b):
+    return a *b
+
+def chu(a,b):
+    if  b == 0:
+        print("分母不能为0")
+    else:
+        return a / b
+
+print("ok")
